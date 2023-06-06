cf7028483f7daa7338156fb2e5909ac4d20117f3
pepno <yizheliu035@gmail.com>
2023-06-06 23:33:15 +0800

    learning method

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
+type kelvin float64
+type celsius float64
+
 func main() {
-	for count := 10; count > 0; count-- {
-		fmt.Println(count)
-	}
+	var k kelvin = 294.0
+	var c celsius
+
+	c = kelvinToCelsius(k)
+	c = k.celsius()
+
+	fmt.Printf("%v\n", c)
+}
+func kelvinToCelsius(k kelvin) celsius {
+	return celsius(k - 273.15)
+}
+
+func (k kelvin) celsius() celsius {
+	return celsius(k - 273.5)
 }
