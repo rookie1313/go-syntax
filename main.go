f964fe501758162660fa3ac739b0c3498024aa55
pepno <yizheliu035@gmail.com>
2023-06-09 13:31:34 +0800

    method

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type kelvin float64
-type celsius float64
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
 
 func main() {
-	var k kelvin = 294.0
-	var c celsius
+	r1 := Rectangle{12, 2}
+	r2 := Rectangle{9, 4}
+	c1 := Circle{10}
+	c2 := Circle{25}
 
-	c = kelvinToCelsius(k)
-	c = k.celsius()
-
-	fmt.Printf("%v\n", c)
-}
-func kelvinToCelsius(k kelvin) celsius {
-	return celsius(k - 273.15)
-}
-
-func (k kelvin) celsius() celsius {
-	return celsius(k - 273.5)
+	fmt.Println("Area of r1 is: ", r1.area())
+	fmt.Println("Area of r2 is: ", r2.area())
+	fmt.Println("Area of c1 is: ", c1.area())
+	fmt.Println("Area of c2 is: ", c2.area())
 }
