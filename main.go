99107c93529b1daf19c47c4a2417674b1931c250
pepno <yizheliu035@gmail.com>
2023-06-09 16:37:44 +0800

    update

--- main.go
+++ main.go
@@ -1,35 +1,75 @@
 package main
 
-import (
-	"fmt"
-	"math"
+import "fmt"
+
+const (
+	WHITE = iota
+	BLACK
+	BLUE
+	RED
+	YELLOW
 )
 
-type Rectangle struct {
-	width  float64
-	height float64
+type Color int
+
+type Box struct {
+	width, height, depth float64
+	color                Color
 }
 
-type Circle struct {
-	radius float64
+type BoxList []Box //a slice of boxes
+
+func (b Box) Volume() float64 {
+	return b.width * b.height * b.depth
 }
 
-func (r Rectangle) area() float64 {
-	return r.width * r.height
+func (b *Box) SetColor(c Color) {
+	b.color = c
 }
 
-func (c Circle) area() float64 {
-	return c.radius * c.radius * math.Pi
+func (bl BoxList) BiggestColor() Color {
+	//init volume and color
+	v := 0.00
+	k := Color(WHITE)
+
+	for _, b := range bl {
+		if bv := b.Volume(); bv > v {
+			v = bv
+			k = b.color
+		}
+	}
+	return k
+}
+
+func (bl BoxList) PaintItBlack() {
+	for i := range bl {
+		bl[i].SetColor(BLACK)
+	}
+}
+
+func (c Color) StringInfo() string {
+	strings := []string{"WHITE", "BLACK", "BLUE", "RED", "YELLOW"}
+	return strings[c]
 }
 
 func main() {
-	r1 := Rectangle{12, 2}
-	r2 := Rectangle{9, 4}
-	c1 := Circle{10}
-	c2 := Circle{25}
+	boxes := BoxList{
+		Box{4, 4, 4, RED},
+		Box{10, 10, 1, YELLOW},
+		Box{1, 1, 20, BLACK},
+		Box{10, 10, 1, BLUE},
+		Box{10, 30, 1, WHITE},
+		Box{20, 20, 20, YELLOW},
+	}
 
-	fmt.Println("Area of r1 is: ", r1.area())
-	fmt.Println("Area of r2 is: ", r2.area())
-	fmt.Println("Area of c1 is: ", c1.area())
-	fmt.Println("Area of c2 is: ", c2.area())
+	fmt.Printf("We have %d boxes in our set\n", len(boxes))
+	fmt.Println("The volume of the first one is", boxes[0].Volume(), "cm³")
+	fmt.Println("The color of the last one is", boxes[len(boxes)-1].color.StringInfo())
+	fmt.Println("The biggest one is", boxes.BiggestColor().StringInfo())
+
+	fmt.Println("Let's paint them all black")
+	boxes.PaintItBlack()
+	fmt.Println("The color of the second one is", boxes[1].color.StringInfo())
+
+	fmt.Println("Obviously, now, the biggest one is", boxes.BiggestColor().StringInfo())
 }
