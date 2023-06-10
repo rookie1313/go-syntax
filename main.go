c175354ec584c3de5de1f2fb58783487c2f24e28
pepno <yizheliu035@gmail.com>
2023-06-10 23:24:25 +0800

    interface

--- main.go
+++ main.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strconv"
 )
 
+type Element interface{}
+type List []Element
+
+type Person struct {
+	name string
+	age  int
+}
+
+//定义了String方法，实现了fmt.Stringer
+func (p Person) String() string {
+	return "(name: " + p.name + " - age: " + strconv.Itoa(p.age) + " years)"
+}
+
 func main() {
-	isPanic := throwsPanic(f)
-	fmt.Println(isPanic)
-}
+	list := make(List, 3)
+	list[0] = 1       // an int
+	list[1] = "Hello" // a string
+	list[2] = Person{"Dennis", 70}
 
-func f() {
-	var user = os.Getenv("USER")
-	if user == "pro" {
-		panic("test for panic")
-	}
-}
-
-func throwsPanic(f func()) (b bool) {
-	defer func() {
-		if x := recover(); x != nil {
-			b = true
+	for index, element := range list {
+		if value, ok := element.(int); ok {
+			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
+		} else if value, ok := element.(string); ok {
+			fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
+		} else if value, ok := element.(Person); ok {
+			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
+		} else {
+			fmt.Printf("list[%d] is of a different type\n", index)
 		}
-	}()
-	f()
-	return
+	}
 }
