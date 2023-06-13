b43e1358b52917fc5a0e703d967887f0cca10fdf
pepno <yizheliu035@gmail.com>
2023-06-13 14:14:41 +0800

    using gorm sqlite3

--- main.go
+++ main.go
@@ -1,110 +1,43 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 	"os"
 
-	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
-	os.Remove("./test.db")
+	os.Remove("test.db")
 
-	db, err := sql.Open("sqlite3", "./test.db")
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	sqlStmt := `
-	create table test (id integer not null primary key, name text);
-	delete from test;
-	`
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
+		panic("failed to connect database")
 	}
 
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-	stmt, err := tx.Prepare("insert into test(id, name) values(?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(i, fmt.Sprintf("data %03d", i))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	err = tx.Commit()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// 迁移 schema
+	db.AutoMigrate(&Product{})
 
-	rows, err := db.Query("select id, name from test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
 
-	stmt, err = db.Prepare("select name from test where id = ?")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	var name string
-	err = stmt.QueryRow("3").Scan(&name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(name)
+	// Read
+	var product Product
+	db.First(&product, 1)                 // 根据整型主键查找
+	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
 
-	_, err = db.Exec("delete from test")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Update - 将 product 的 price 更新为 200
+	db.Model(&product).Update("Price", 200)
+	// Update - 更新多个字段
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 
-	_, err = db.Exec("insert into test(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err = db.Query("select id, name from test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Delete - 删除 product
+	db.Delete(&product, 1)
 }
