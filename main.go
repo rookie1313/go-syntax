1030c5ce0ffdd36d255819a4da7ffcf50235140d
pepno <yizheliu035@gmail.com>
2023-06-13 10:04:18 +0800

    sqlite3

--- main.go
+++ main.go
@@ -1,43 +1,110 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
-	"strings"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// handler function()
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()                         // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form)                   // print form information in server side
-	fmt.Println("path :", r.URL.Path)     // print request path
-	fmt.Println("scheme: ", r.URL.Scheme) // print request scheme
-	fmt.Println(r.Form["url_long"])       // print form information
-
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello test!")
-}
-
-// func CommandLineHandler(writer http.ResponseWriter, request *http.Request) {
-// 	url := request.URL
-// 	query := url.Query()
-
-// 	id := query["id"]
-// 	for i := 0; i < len(id); i++ {
-// 		log.Printf("id is %s\n", id[i])
-// 	}
-
-// 	name := query.Get("name")
-// 	log.Println("first name is", name)
-// }
-
 func main() {
-	//HandleFunc registers the handler function for the given pattern in the DefaultServeMux.
-	http.HandleFunc("/urlinfo", testHandler)
+	os.Remove("./test.db")
 
-	//ListenAndServe listens on the TCP network address addr and then calls Serve with handler to handle requests on incoming connections.
-	http.ListenAndServe("localhost:8080", nil)
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table test (id integer not null primary key, name text);
+	delete from test;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("insert into test(id, name) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	for i := 0; i < 100; i++ {
+		_, err = stmt.Exec(i, fmt.Sprintf("data %03d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("select id, name from test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err = db.Prepare("select name from test where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var name string
+	err = stmt.QueryRow("3").Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(name)
+
+	_, err = db.Exec("delete from test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("insert into test(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = db.Query("select id, name from test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
