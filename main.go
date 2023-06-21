c28c2b6626cc93e63ecdb26d51d30207c994b490
pepno <yizheliu035@gmail.com>
2023-06-21 22:03:48 +0800

    multiple response

--- main.go
+++ main.go
@@ -1,43 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/testdata/protoexample"
 	"net/http"
-	"strings"
 )
 
-// handler function()
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()                         // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form)                   // print form information in server side
-	fmt.Println("path :", r.URL.Path)     // print request path
-	fmt.Println("scheme: ", r.URL.Scheme) // print request scheme
-	fmt.Println(r.Form["url_long"])       // print form information
-
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello test!")
-}
-
-// func CommandLineHandler(writer http.ResponseWriter, request *http.Request) {
-// 	url := request.URL
-// 	query := url.Query()
-
-// 	id := query["id"]
-// 	for i := 0; i < len(id); i++ {
-// 		log.Printf("id is %s\n", id[i])
-// 	}
-
-// 	name := query.Get("name")
-// 	log.Println("first name is", name)
-// }
-
+// 多种响应方式
 func main() {
-	//HandleFunc registers the handler function for the given pattern in the DefaultServeMux.
-	http.HandleFunc("/urlinfo", testHandler)
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	// 1.json
+	r.GET("/someJSON", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "someJSON", "status": 200})
+	})
+	// 2. struct响应
+	r.GET("/someStruct", func(c *gin.Context) {
+		var msg struct {
+			Name   string
+			Text   string
+			Number int
+		}
+		msg.Name = "root"
+		msg.Text = "text"
+		msg.Number = 1
+		c.JSON(http.StatusOK, msg)
+	})
+	//test
+	r.GET("/Test", func(c *gin.Context) {
+		var msg struct {
+			T string
+		}
+		msg.T = "something"
+		c.JSON(http.StatusOK, gin.H{"Literal": msg})
+	})
+	// 3.XML
+	r.GET("/someXML", func(c *gin.Context) {
+		c.XML(http.StatusOK, gin.H{"message": "abc"})
+	})
+	// 4.YAML响应
+	r.GET("/someYAML", func(c *gin.Context) {
+		c.YAML(http.StatusOK, gin.H{"name": "jack"})
+	})
+	// 5.protobuf格式,谷歌开发的高效存储读取的工具
+	// 数组？切片？如果自己构建一个传输格式，应该是什么格式？
+	r.GET("/someProtoBuf", func(c *gin.Context) {
+		reps := []int64{int64(1), int64(2)}
+		// 定义数据
+		label := "label"
+		// 传protobuf格式数据
+		data := &protoexample.Test{
+			Label: &label,
+			Reps:  reps,
+		}
+		c.ProtoBuf(http.StatusOK, data)
+	})
 
-	//ListenAndServe listens on the TCP network address addr and then calls Serve with handler to handle requests on incoming connections.
-	http.ListenAndServe("localhost:8080", nil)
+	r.Run(":8080")
 }
