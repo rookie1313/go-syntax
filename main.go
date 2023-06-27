00027d6a2fa81187eab88bc5fe6ece5d25de4e28
pepno <yizheliu035@gmail.com>
2023-06-27 12:54:34 +0800

    tcp sender

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
 )
 
+// 客户端
 func main() {
-	// 创建Gin引擎实例
-	router := gin.Default()
-
-	// 定义异步GET请求的路由处理函数
-	router.GET("/async", func(c *gin.Context) {
-		// 启动一个Goroutine执行异步任务
-		go func() {
-			//log.Println("异步执行：" + copyContext.Request.URL.String())
-			c.JSON(http.StatusOK, "异步执行")
-		}()
-
-		// 返回异步任务已启动的消息
-		c.JSON(http.StatusOK, "Async task started")
-	})
-
-	// 运行Gin引擎
-	router.Run(":8080")
+	conn, err := net.Dial("tcp", ":5566")
+	if err != nil {
+		fmt.Println("err :", err)
+		return
+	}
+	defer conn.Close() // 关闭连接
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := inputReader.ReadString('\n') // 读取用户输入
+		inputInfo := strings.Trim(input, "\r\n")
+		if strings.ToUpper(inputInfo) == "Q" { // 如果输入q就退出
+			return
+		}
+		_, err = conn.Write([]byte(inputInfo)) // 发送数据
+		if err != nil {
+			return
+		}
+		buf := [512]byte{}
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("recv failed, err:", err)
+			return
+		}
+		fmt.Println(string(buf[:n]))
+	}
 }
