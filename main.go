74d915833ff58fe7a2c019637e5d599fa85c8e9f
pepno <yizheliu035@gmail.com>
2023-07-08 19:45:14 +0800

    stack implemented by slice

--- main.go
+++ main.go
@@ -1,58 +1,54 @@
 package main
 
-import "container/list"
-
 func main() {
 }
 
-/* 基于链表实现的栈 */
-type linkedListStack struct {
-	// 使用内置包 list 来实现栈
-	data *list.List
+/* 基于数组实现的栈 */
+type arrayStack struct {
+	data []int // 数据
 }
 
 /* 初始化栈 */
-func newLinkedListStack() *linkedListStack {
-	return &linkedListStack{
-		data: list.New(),
+func newArrayStack() *arrayStack {
+	return &arrayStack{
+		// 设置栈的长度为 0，容量为 16
+		data: make([]int, 0, 16),
 	}
+}
+
+/* 栈的长度 */
+func (s *arrayStack) size() int {
+	return len(s.data)
+}
+
+/* 栈是否为空 */
+func (s *arrayStack) isEmpty() bool {
+	return s.size() == 0
 }
 
 /* 入栈 */
-func (s *linkedListStack) push(value int) {
-	s.data.PushBack(value)
+func (s *arrayStack) push(v int) {
+	// 切片会自动扩容
+	s.data = append(s.data, v)
 }
 
 /* 出栈 */
-func (s *linkedListStack) pop() any {
+func (s *arrayStack) pop() any {
+	val := s.peek()
+	s.data = s.data[:len(s.data)-1]
+	return val
+}
+
+/* 获取栈顶元素 */
+func (s *arrayStack) peek() any {
 	if s.isEmpty() {
 		return nil
 	}
-	e := s.data.Back()
-	s.data.Remove(e)
-	return e.Value
+	val := s.data[len(s.data)-1]
+	return val
 }
 
-/* 访问栈顶元素 */
-func (s *linkedListStack) peek() any {
-	if s.isEmpty() {
-		return nil
-	}
-	e := s.data.Back()
-	return e.Value
-}
-
-/* 获取栈的长度 */
-func (s *linkedListStack) size() int {
-	return s.data.Len()
-}
-
-/* 判断栈是否为空 */
-func (s *linkedListStack) isEmpty() bool {
-	return s.data.Len() == 0
-}
-
-/* 获取 List 用于打印 */
-func (s *linkedListStack) toList() *list.List {
+/* 获取 Slice 用于打印 */
+func (s *arrayStack) toSlice() []int {
 	return s.data
 }
