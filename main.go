f15a21ff9d8419704aa243a56403ff4b806e666f
pepno <yizheliu035@gmail.com>
2023-07-09 01:18:30 +0800

    tree defination and traversal

--- main.go
+++ main.go
@@ -1,58 +1,46 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+)
 
 func main() {
 }
 
-/* 基于链表实现的队列 */
-type linkedListQueue struct {
-	// 使用内置包 list 来实现队列
-	data *list.List
-}
-
-/* 初始化队列 */
-func newLinkedListQueue() *linkedListQueue {
-	return &linkedListQueue{
-		data: list.New(),
+func NewTreeNode(value int) *TreeNode {
+	return &TreeNode{
+		Val:   value,
+		Left:  nil,
+		Right: nil,
 	}
 }
 
-/* 入队 */
-func (s *linkedListQueue) push(value any) {
-	s.data.PushBack(value)
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-/* 出队 */
-func (s *linkedListQueue) pop() any {
-	if s.isEmpty() {
-		return nil
+/* 层序遍历 */
+func levelOrder(root *TreeNode) []int {
+	// 初始化队列，加入根节点
+	queue := list.New()
+	queue.PushBack(root)
+	// 初始化一个切片，用于保存遍历序列
+	nums := make([]int, 0)
+	for queue.Len() > 0 {
+		// 队列出队
+		node := queue.Remove(queue.Front()).(*TreeNode)
+		// 保存节点值
+		nums = append(nums, node.Val)
+		if node.Left != nil {
+			// 左子节点入队
+			queue.PushBack(node.Left)
+		}
+		if node.Right != nil {
+			// 右子节点入队
+			queue.PushBack(node.Right)
+		}
 	}
-	e := s.data.Front()
-	s.data.Remove(e)
-	return e.Value
-}
-
-/* 访问队首元素 */
-func (s *linkedListQueue) peek() any {
-	if s.isEmpty() {
-		return nil
-	}
-	e := s.data.Front()
-	return e.Value
-}
-
-/* 获取队列的长度 */
-func (s *linkedListQueue) size() int {
-	return s.data.Len()
-}
-
-/* 判断队列是否为空 */
-func (s *linkedListQueue) isEmpty() bool {
-	return s.data.Len() == 0
-}
-
-/* 获取 List 用于打印 */
-func (s *linkedListQueue) toList() *list.List {
-	return s.data
+	return nums
 }
