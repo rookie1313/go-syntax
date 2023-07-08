982579f32fe136e61036abc52444368ec4add1ba
pepno <yizheliu035@gmail.com>
2023-07-08 21:42:46 +0800

    queue implemented by linked list

--- main.go
+++ main.go
@@ -1,54 +1,58 @@
 package main
 
+import "container/list"
+
 func main() {
 }
 
-/* 基于数组实现的栈 */
-type arrayStack struct {
-	data []int // 数据
+/* 基于链表实现的队列 */
+type linkedListQueue struct {
+	// 使用内置包 list 来实现队列
+	data *list.List
 }
 
-/* 初始化栈 */
-func newArrayStack() *arrayStack {
-	return &arrayStack{
-		// 设置栈的长度为 0，容量为 16
-		data: make([]int, 0, 16),
+/* 初始化队列 */
+func newLinkedListQueue() *linkedListQueue {
+	return &linkedListQueue{
+		data: list.New(),
 	}
 }
 
-/* 栈的长度 */
-func (s *arrayStack) size() int {
-	return len(s.data)
+/* 入队 */
+func (s *linkedListQueue) push(value any) {
+	s.data.PushBack(value)
 }
 
-/* 栈是否为空 */
-func (s *arrayStack) isEmpty() bool {
-	return s.size() == 0
-}
-
-/* 入栈 */
-func (s *arrayStack) push(v int) {
-	// 切片会自动扩容
-	s.data = append(s.data, v)
-}
-
-/* 出栈 */
-func (s *arrayStack) pop() any {
-	val := s.peek()
-	s.data = s.data[:len(s.data)-1]
-	return val
-}
-
-/* 获取栈顶元素 */
-func (s *arrayStack) peek() any {
+/* 出队 */
+func (s *linkedListQueue) pop() any {
 	if s.isEmpty() {
 		return nil
 	}
-	val := s.data[len(s.data)-1]
-	return val
+	e := s.data.Front()
+	s.data.Remove(e)
+	return e.Value
 }
 
-/* 获取 Slice 用于打印 */
-func (s *arrayStack) toSlice() []int {
+/* 访问队首元素 */
+func (s *linkedListQueue) peek() any {
+	if s.isEmpty() {
+		return nil
+	}
+	e := s.data.Front()
+	return e.Value
+}
+
+/* 获取队列的长度 */
+func (s *linkedListQueue) size() int {
+	return s.data.Len()
+}
+
+/* 判断队列是否为空 */
+func (s *linkedListQueue) isEmpty() bool {
+	return s.data.Len() == 0
+}
+
+/* 获取 List 用于打印 */
+func (s *linkedListQueue) toList() *list.List {
 	return s.data
 }
