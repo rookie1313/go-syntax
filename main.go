4c766b3c1bff5f2f594df09a49d26aac27bbcb0d
pepno <yizheliu035@gmail.com>
2023-11-21 15:03:51 +0800

    select tdd
    Signed-off-by: pepno <yizheliu035@gmail.com>

--- main.go
+++ main.go
@@ -1,43 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"strings"
-)
-
-// handler function()
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()                         // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form)                   // print form information in server side
-	fmt.Println("path :", r.URL.Path)     // print request path
-	fmt.Println("scheme: ", r.URL.Scheme) // print request scheme
-	fmt.Println(r.Form["url_long"])       // print form information
-
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello test!")
-}
-
-// func CommandLineHandler(writer http.ResponseWriter, request *http.Request) {
-// 	url := request.URL
-// 	query := url.Query()
-
-// 	id := query["id"]
-// 	for i := 0; i < len(id); i++ {
-// 		log.Printf("id is %s\n", id[i])
-// 	}
-
-// 	name := query.Get("name")
-// 	log.Println("first name is", name)
-// }
-
 func main() {
-	//HandleFunc registers the handler function for the given pattern in the DefaultServeMux.
-	http.HandleFunc("/urlinfo", testHandler)
-
-	//ListenAndServe listens on the TCP network address addr and then calls Serve with handler to handle requests on incoming connections.
-	http.ListenAndServe("localhost:8080", nil)
 }
---
+++ select_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func TestRacer(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if want != got {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		ch <- struct{}{}
+	}()
+
+	return ch
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
