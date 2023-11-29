ba6e1f86694c26ca4c473c0fab3e181dcab8298e
pepno <yizheliu035@gmail.com>
2023-11-29 22:22:24 +0800

    property based refactor
    Signed-off-by: pepno <yizheliu035@gmail.com>

--- roman_numerals_test.go
+++ roman_numerals_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"testing/quick"
 )
 
 type RomanNumeral struct {
@@ -97,34 +98,47 @@ func TestConvertingToArabic(t *testing.T) {
 	}
 }
 
-func ConvertToArabic(roman string) int {
-	var total int
+type windowedRoman string
 
-	for i := 0; i < len(roman); i++ {
-		symbol := roman[i]
-		if couldBeSubtractive(i, symbol, roman) {
-			// get the value of the two character string
-			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
-				total += value
-				i++
-			} else {
-				total += allRomanNumerals.ValueOf(symbol)
-			}
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
 
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{symbol, w[i+1]})
+			i++
 		} else {
-			total += allRomanNumerals.ValueOf(symbol)
+			symbols = append(symbols, []byte{symbol})
 		}
-
 	}
-	return total
+
+	return
 }
 
-func couldBeSubtractive(index int, currentSymbol byte, roman string) bool {
-	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
-	return index+1 < len(roman) && isSubtractiveSymbol
+func ConvertToArabic(roman string) (total int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
+	}
+	return
+}
+
+func isSubtractive(symbol byte) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
 }
 
 type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+
+	return false
+}
 
 func (r RomanNumerals) ValueOf(symbols ...byte) int {
 	symbol := string(symbols)
@@ -136,4 +150,23 @@ func (r RomanNumerals) ValueOf(symbols ...byte) int {
 	}
 
 	return 0
+}
+
+// property base tests
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(int(arabic))
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == int(arabic)
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
+		t.Error("failed checks", err)
+	}
 }
